db79f61ea084f6f4fe5a6a1545b92e1643e98388
stepikhw <107130876+stepikhw@users.noreply.github.com>
2022-06-10 07:44:18 -0700

    Removed unused comment

--- pages/product_page.py
+++ pages/product_page.py
@@ -10,23 +10,6 @@ class ProductPage(BasePage):
         self.should_be_product_page()
         self.add_product_to_basket()
 
-    # def should_be_login_page(self):
-    #     self.should_be_login_url()
-    #     self.should_be_login_form()
-    #     self.should_be_register_form()
-    #
-    # def should_be_login_url(self):
-    #     # реализуйте проверку на корректный url адрес
-    #     current_url = self.browser.current_url
-    #     assert "login" in current_url, f"URL [\"{current_url}\"] does not look like a login URL."
-    #
-    # def should_be_login_form(self):
-    #     # реализуйте проверку, что есть форма логина
-    #     assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form doesn't present on the page."
-    #
-    # def should_be_register_form(self):
-    #     # реализуйте проверку, что есть форма регистрации на странице
-    #     assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form doesn't present on the page."
     def should_be_product_page(self):
         self.should_have_product_description()
 
