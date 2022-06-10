c214f680f076138f896745b0a605bb8749550135
stepikhw <107130876+stepikhw@users.noreply.github.com>
2022-06-10 04:29:26 -0700

    Implemented class for login page, added locators for it

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,11 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM=(By.CSS_SELECTOR,"form#login_form")
+    REGISTER_FORM=(By.CSS_SELECTOR,"form#register_form")
+    # LOGIN_USERNAME_INPUT=(By.CSS_SELECTOR,"form#login_form input#id_login-username[required]")
+    # LOGIN_PASSWORD_INPUT=(By.CSS_SELECTOR,"form#login_form input#id_login-username[required]")
+    # PASSWORD_RESET_LINK=(By.CSS_SELECTOR,"form#login_form a[href$=\"/password-reset/\"]")
+    # LOGIN_BUTTON = (By.CSS_SELECTOR, "form#login_form button[name=\"login_submit\"]")
---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        current_url = self.browser.current_url
+        assert "login" in current_url, f"URL [\"{current_url}\"] does not look like a login URL."
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form doesn't present on the page."
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form doesn't present on the page."
